c65bcc834e0f8a79ed5884f1262573e5386feeb4
mahovsson <59553868+mahovsson@users.noreply.github.com>
2021-06-21 09:22:11 +0200

    Add files via upload

---
+++ src/components/Timeline.vue
@@ -0,0 +1,34 @@
+<template>
+	<vertical-timeline :inputData="data" :reversed="false" />
+</template>
+
+<script>
+import data from '../data/data.json';
+import VerticalTimeline from './VerticalTimeline.vue';
+
+export default {
+	components: { VerticalTimeline },
+	data() {
+		return {
+			data: data,
+		};
+	},
+};
+</script>
+
+<style scss>
+h3 {
+	margin: 10px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
